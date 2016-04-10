c3d415625d3b797bc7352d301ff4fbd38e580ba7
jmsalcido <eljose152@gmail.com>
2016-04-09 17:45:35 -0700

    Initial Commit

---
+++ ScaryBugsMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ScaryBugsMac
+//
+//  Created by Jose Miguel Salcido on 4/9/16.
+//  Copyright © 2016 Jose Miguel Salcido. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
