b93644b18e9638cce2fda6eeda376581fc397eb0
jmsalcido <eljose152@gmail.com>
2016-04-09 18:30:46 -0700

    updates

--- ScaryBugsMac/ScaryBugData.swift
+++ ScaryBugsMac/ScaryBugData.swift
@@ -9,5 +9,16 @@
 import Cocoa
 
 class ScaryBugData: NSObject {
-
+    var title: String
+    var rating: Double
+    
+    override init() {
+        self.title = String()
+        self.rating = 0.0
+    }
+    
+    init(title: String, rating: Double) {
+        self.title = title
+        self.rating = rating
+    }
 }
--- ScaryBugsMac/ScaryBugDoc.swift
+++ ScaryBugsMac/ScaryBugDoc.swift
@@ -9,5 +9,18 @@
 import Cocoa
 
 class ScaryBugDoc: NSObject {
-
+    var data: ScaryBugData
+    var thumbImage: NSImage?
+    var fullImage: NSImage?
+    
+    override init() {
+        self.data = ScaryBugData()
+    }
+    
+    init(title: String, rating: Double, thumbImage: NSImage?, fullImage: NSImage?) {
+        self.data = ScaryBugData(title: title, rating: rating)
+        self.thumbImage = thumbImage
+        self.fullImage = fullImage
+    }
+    
 }
