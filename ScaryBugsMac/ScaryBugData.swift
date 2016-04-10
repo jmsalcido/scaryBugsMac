0fa553ae192b9d87d8e03a276c1efe76b7b32fa5
jmsalcido <eljose152@gmail.com>
2016-04-09 18:30:27 -0700

    adding new model

---
+++ ScaryBugsMac/ScaryBugData.swift
@@ -0,0 +1,13 @@
+//
+//  ScaryBugData.swift
+//  ScaryBugsMac
+//
+//  Created by Jose Miguel Salcido on 4/9/16.
+//  Copyright © 2016 Jose Miguel Salcido. All rights reserved.
+//
+
+import Cocoa
+
+class ScaryBugData: NSObject {
+
+}
---
+++ ScaryBugsMac/ScaryBugDoc.swift
@@ -0,0 +1,13 @@
+//
+//  ScaryBugDoc.swift
+//  ScaryBugsMac
+//
+//  Created by Jose Miguel Salcido on 4/9/16.
+//  Copyright © 2016 Jose Miguel Salcido. All rights reserved.
+//
+
+import Cocoa
+
+class ScaryBugDoc: NSObject {
+
+}
