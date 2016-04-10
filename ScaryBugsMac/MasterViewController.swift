bb0aa9abcc28b9e823b9ca3d5c0fdc20991ddf74
jmsalcido <eljose152@gmail.com>
2016-04-09 17:50:47 -0700

    first draft

---
+++ ScaryBugsMac/MasterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.swift
+//  ScaryBugsMac
+//
+//  Created by Jose Miguel Salcido on 4/9/16.
+//  Copyright © 2016 Jose Miguel Salcido. All rights reserved.
+//
+
+import Cocoa
+
+class MasterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
