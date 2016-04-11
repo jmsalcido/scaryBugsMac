525ea68253328c8a4064f93728a9fd4511b878fb
jmsalcido <eljose152@gmail.com>
2016-04-10 18:33:22 -0700

    Add stub data

--- ScaryBugsMac/MasterViewController.swift
+++ ScaryBugsMac/MasterViewController.swift
@@ -9,12 +9,28 @@
 import Cocoa
 
 class MasterViewController: NSViewController {
-
+    
+    let useStub = true
     var bugs = [ScaryBugDoc]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        bugs = getBugs()
+    }
+    
+    func getBugs() -> [ScaryBugDoc] {
+        if useStub {
+            return setupSampleBugs();
+        }
+        return [ScaryBugDoc]()
+    }
+    
+    func setupSampleBugs() -> [ScaryBugDoc] {
+        let stubBug1 = ScaryBugDoc(title: "Potato Bug", rating: 4.0, thumbImage: NSImage(named: "potatoBugThumb"), fullImage: NSImage(named: "potatoBug"))
+        let stubBug2 = ScaryBugDoc(title: "House Centipede", rating: 3.0, thumbImage: NSImage(named: "centipedeThumb"), fullImage: NSImage(named: "centipede"))
+        let stubBug3 = ScaryBugDoc(title: "Wolf Spider", rating: 5.0, thumbImage: NSImage(named: "wolfSpiderThumb"), fullImage: NSImage(named: "wolfSpider"))
+        let stubBug4 = ScaryBugDoc(title: "Lady Bug", rating: 1.0, thumbImage: NSImage(named: "ladyBugThumb"), fullImage: NSImage(named: "ladyBug"))
+        return [stubBug1, stubBug2, stubBug3, stubBug4]
     }
     
 }
